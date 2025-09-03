92693eff3c6c80ad617fa7673be074a90458899e
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2025-09-03 07:33:18 +0530

    application.properties and configurations edited

--- .history/src/main/java/com/karimi/note_app_backend/configuration/NoteWebConfiguration_20250902054959.java
+++ .history/src/main/java/com/karimi/note_app_backend/configuration/NoteWebConfiguration_20250902054959.java
@@ -9,7 +9,7 @@ public class NoteWebConfiguration implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/notes/**")
-        .allowedOrigins("https://note-app-kappa-flax.vercel.app")
+        .allowedOrigins("http://localhost:5173","http://192.168.1.26:5173")
 
         .allowedMethods("GET","POST","PUT","DELETE");
     }
